b816e27bb7198e151a9bd58887e73b5801b46630
Erin Jerri Pangilinan <3184381+erinjerri@users.noreply.github.com>
2026-03-03 23:24:03 -0800

    added Google Analytics script tag

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 
 import { cn } from '@/utilities/ui'
 import React from 'react'
+import Script from 'next/script'
 import { Jost, League_Spartan } from 'next/font/google'
 
 import { AdminBar } from '@/components/AdminBar'
@@ -58,6 +59,22 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           <Footer />
         </Providers>
       </body>
+      {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+            `}
+          </Script>
+        </>
+      )}
     </html>
   )
 }
